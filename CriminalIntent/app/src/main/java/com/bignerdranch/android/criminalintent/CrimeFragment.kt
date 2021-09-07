e22cca9d95402ab610d882a01ad7d9bf060d2a6d
Oleg Dontsov <o.dontsov@samsung.com>
2021-09-07 20:01:07 +0300

    Day 15

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -1,14 +1,23 @@
 package com.bignerdranch.android.criminalintent
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.text.Editable
 import android.text.TextWatcher
+import android.text.format.DateFormat
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
@@ -20,6 +29,7 @@ private const val ARG_CRIME_ID = "crime_id"
 private const val DIALOG_DATE = "DialogDate"
 const val REQUEST_DATE_KEY = "REQUEST_DATE_KEY"
 const val REQUEST_DATE_BUNDLE = "REQUEST_DATE_BUNDLE"
+private const val DATE_FORMAT = "EEE, MMM, dd"
 
 class CrimeFragment : Fragment() {
 
@@ -27,6 +37,28 @@ class CrimeFragment : Fragment() {
     private lateinit var titleField: EditText
     private lateinit var dateButton: Button
     private lateinit var solvedCheckBox: CheckBox
+    private lateinit var reportButton: Button
+    private lateinit var suspectButton: Button
+
+    private val getContent = registerForActivityResult(ActivityResultContracts.PickContact()) { contactUri: Uri? ->
+        // Указать, для каких полей ваш запрос должен возвращать значения.
+        val queryFields = arrayOf(ContactsContract.Contacts.DISPLAY_NAME)
+        // Выполняемый здесь запрос — contactUri похож на предложение "where"
+        val cursor = contactUri?.let {
+            requireActivity().contentResolver.query(it, queryFields, null, null, null)
+        }
+        cursor?.use {
+            // Verify cursor contains at least one result
+            if (it.count != 0) {
+            // Первый столбец первой строки данных — это имя вашего подозреваемого.
+                it.moveToFirst()
+                val suspect = it.getString(0)
+                crime.suspect = suspect
+                crimeDetailViewModel.saveCrime(crime)
+                suspectButton.text = suspect
+            }
+        }
+    }
 
     private val crimeDetailViewModel: CrimeDetailViewModel by viewModels()
 
@@ -47,6 +79,8 @@ class CrimeFragment : Fragment() {
         titleField = view.findViewById(R.id.crime_title) as EditText
         dateButton = view.findViewById(R.id.crime_date) as Button
         solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
+        reportButton = view.findViewById(R.id.crime_report) as Button
+        suspectButton = view.findViewById(R.id.crime_suspect) as Button
 
         return view
     }
@@ -106,6 +140,34 @@ class CrimeFragment : Fragment() {
             }
         }
 
+        reportButton.setOnClickListener {
+            Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, getCrimeReport())
+                putExtra(
+                    Intent.EXTRA_SUBJECT,
+                    getString(R.string.crime_report_subject))
+            }.also { intent ->
+                startActivity(Intent.createChooser(intent, "Gmail"))
+            }
+        }
+
+        suspectButton.apply {
+            setOnClickListener { getContent.launch() }
+
+            val pickContactIntent =
+                Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+            val packageManager: PackageManager = requireActivity().packageManager
+            val resolvedActivity: ResolveInfo? =
+                packageManager.resolveActivity(pickContactIntent,
+                    PackageManager.MATCH_DEFAULT_ONLY)
+
+            Log.e(TAG, "suspectButton.apply: resolvedActivity222=" + resolvedActivity)
+            if (resolvedActivity == null) {
+                isEnabled = false
+            }
+        }
+
         setFragmentResultListener(REQUEST_DATE_KEY) { _, bundle ->
             // We use a String here, but any type that can be put in a Bundle is supported
             val result = bundle.getLong(REQUEST_DATE_BUNDLE)
@@ -127,6 +189,26 @@ class CrimeFragment : Fragment() {
             isChecked = crime.isSolved
             jumpDrawablesToCurrentState()
         }
+
+        if (crime.suspect.isNotEmpty()) {
+            suspectButton.text = crime.suspect
+        }
+    }
+
+    private fun getCrimeReport(): String {
+        val solvedString = if (crime.isSolved) {
+            getString(R.string.crime_report_solved)
+        } else {
+            getString(R.string.crime_report_unsolved)
+        }
+        val dateString = DateFormat.format(DATE_FORMAT, crime.date).toString()
+        var suspect = if (crime.suspect.isBlank()) {
+            getString(R.string.crime_report_no_suspect)
+        } else {
+            getString(R.string.crime_report_suspect, crime.suspect)
+        }
+        return getString(R.string.crime_report,
+            crime.title, dateString, solvedString, suspect)
     }
 
     companion object {
--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.Room
 import com.bignerdranch.android.criminalintent.database.CrimeDatabase
+import com.bignerdranch.android.criminalintent.database.migration_1_2
 import java.util.*
 import java.util.concurrent.Executors
 
@@ -11,11 +12,11 @@ private const val DATABASE_NAME = "crime-database"
 
 class CrimeRepository private constructor(context: Context) {
 
-    private val database : CrimeDatabase = Room.databaseBuilder(
+    private val database: CrimeDatabase = Room.databaseBuilder(
         context.applicationContext,
         CrimeDatabase::class.java,
         DATABASE_NAME
-    ).build()
+    ).addMigrations(migration_1_2).build()
 
     private val crimeDao = database.crimeDao()
     private val executor = Executors.newSingleThreadExecutor() //remove before pushing
